273b534d78273c8fd7cdce26cd59868b6cbb46fd
XiaoLin <i@amxiaol.in>
2020-03-14 14:34:49 +0800

    feat: support database paginate

--- src/module/database.js
+++ src/module/database.js
@@ -3,5 +3,7 @@ const knex = require('knex')({
     client: "mysql",
     connection: config.get('database'),
 });
+const {attachPaginate} = require('knex-paginate');
+attachPaginate();
 
 module.exports = knex;
