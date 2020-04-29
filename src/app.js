62733776bf6a7f23e96c3f3b044ace264b12465d
XiaoLin <i@amxiaol.in>
2020-04-29 11:47:13 +0800

    feat: sentry error tracking module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
+const Sentry = require('@sentry/node')
+
+Sentry.init({
+    dsn: 'https://a5df6f6888404ec492be93b7e93b5dd3@o230009.ingest.sentry.io/5217379'
+})
+
 require('./api/init')
 require('./import/init')
